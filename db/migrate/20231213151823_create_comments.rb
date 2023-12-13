74f795ebfe43783cbd74dda13efa3ed45e2be1e1
hydaline-code <djougancharlen@gmail.com>
2023-12-13 20:34:22 +0100

    Fix schema file

--- db/migrate/20231213151823_create_comments.rb
+++ db/migrate/20231213151823_create_comments.rb
@@ -1,12 +1,11 @@
 class CreateComments < ActiveRecord::Migration[7.1]
   def change
     create_table :comments do |t|
-      t.references :user, null: false,foreign_key: { to_table: :users }
+      t.references :user, null: false,foreign_key: true
       t.references :post, null: false, foreign_key: true
       t.text :text
 
       t.timestamps
     end
-    add_index :comments, [:user_id, :post_id]
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -34,19 +34,19 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_13_152312) do
   end
 
   create_table "posts", force: :cascade do |t|
+    t.bigint "author_id"
     t.string "title"
-    t.text "content"
+    t.text "text"
     t.integer "comments_counter"
     t.string "likes_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["comments_counter"], name: "index_posts_on_comments_counter"
-    t.index ["likes_counter"], name: "index_posts_on_likes_counter"
+    t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "photo"
+    t.string "photo_link"
     t.text "bio"
     t.integer "posts_counter"
     t.datetime "created_at", null: false
@@ -57,4 +57,5 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_13_152312) do
   add_foreign_key "comments", "users"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
+  add_foreign_key "posts", "users", column: "author_id", name: "author_id"
 end
