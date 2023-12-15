bb80dd24e475763847ba2aedfcef94a0d211d7a8
hydaline-code <djougancharlen@gmail.com>
2023-12-15 02:49:31 +0100

    Process data success

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,11 +1,10 @@
 class Comment < ApplicationRecord
   belongs_to :user
-  belongs_to :post, counter_cache: true
+  belongs_to :post, counter_cache: :comments_counter, optional: true
 
-  after_create :update_post_comments_counter
-  
-  def update_post_comments_counter
+  after_create :update_post_comments_count
+
+  def update_post_comments_count
     post.update(comments_counter: post.comments.count)
   end
-
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -3,9 +3,7 @@ class Like < ApplicationRecord
   belongs_to :post, counter_cache: true
 
   after_create :update_post_likes_counter
-  
   def update_post_likes_counter
     post.update(likes_counter: post.likes.count)
   end
-
 end
