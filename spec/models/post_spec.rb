862a16d8d4f116c39b3595dccb7f6790cb2856e5
hydaline-code <djougancharlen@gmail.com>
2023-12-16 20:17:49 +0100

    update the testing file

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it 'title must exit' do
+      user = User.new(name: 'shahadat', photo_link: 'url', bio: 'developer')
+      post = Post.new(author_id: user.id, title: 'new title', text: 'This is my second post')
+      post.title = nil
+      expect(post).to_not be_valid
+    end
+    it 'length must be less than 250' do
+      user = User.new(name: 'shahadat', photo_link: 'url', bio: 'developer')
+      post = Post.new(author_id: user.id, title: 'new title', text: 'This is my second post')
+      post.text = nil
+      expect(post).to_not be_valid
+    end
+    it 'length must  not be 0' do
+      user = User.new(name: 'shahadat', photo_link: 'url', bio: 'developer')
+      post = Post.new(author_id: user.id, title: 'new title')
+      expect(post).to_not be_valid
+    end
+    it 'comments_counter must be greater than or equal to 0' do
+      user = User.new(name: 'shahadat', photo_link: 'url', bio: 'developer')
+      post = Post.new(author_id: user.id, title: 'new title', text: 'This is my second post', comments_counter: -1)
+      expect(post).to_not be_valid
+    end
+    it 'likes_counter must be equal to 0' do
+      user = User.new(name: 'shahadat', photo_link: 'url', bio: 'developer')
+      post = Post.new(author_id: user.id, title: 'new title', text: 'This is my second post', likes_counter: 1)
+      expect(post).to_not be_valid
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it 'name must be exist' do
+      user = User.new(name: 'shahadat', photo_link: 'url', bio: 'developer')
+      user.name = nil
+      expect(user).to_not be_valid
+    end
+    it 'posts_counter must be greate that 0' do
+      user = User.new(name: 'shahadat', photo_link: 'url', bio: 'developer', posts_counter: -1)
+      expect(user).to_not be_valid
+    end
+    it 'posts_counter must be greate that 0' do
+      user = User.new(name: 'shahadat', photo_link: 'url', bio: 'developer', posts_counter: 0)
+      expect(user).to be_valid
+    end
+    it 'posts_counter must be greate that 0' do
+      user = User.new(name: 'shahadat', photo_link: 'url', bio: 'developer', posts_counter: 1)
+      expect(user).to be_valid
+    end
+    it 'most_recent_posts should return last 3 post' do
+      user = User.create(name: 'chacha', photo_link: 'url', bio: 'developer')
+      Post.create(author_id: user.id, title: 'new title', text: 'This is my second post')
+      post1 = Post.create(author_id: user.id, title: 'new title1', text: 'This is my second post')
+      post2 = Post.create(author_id: user.id, title: 'new title2', text: 'This is my second post')
+      post3 = Post.create(author_id: user.id, title: 'new title3', text: 'This is my second post')
+      most_recent_posts = user.most_recent_posts
+      expect(most_recent_posts).to eq([post1, post2, post3])
+    end
+  end
 end
