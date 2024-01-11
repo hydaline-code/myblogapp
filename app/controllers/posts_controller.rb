d86356a3c6dfbb654ad8196ce3107db9319d8547
hydaline-code <djougancharlen@gmail.com>
2024-01-11 03:34:47 +0100

    updates to stick to the wireframe

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,23 @@
 class PostsController < ApplicationController
+  before_action :find_user
+  before_action :find_post, only: [:show]
+  
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts.includes(:comments)
+  end
+
+  def show
+
+  end
+
+  private
+
+  def find_user
+    @user = User.find(params[:user_id])
+  end 
+
+  def find_post
+    @post = @user.posts.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   # root "posts#index"
 
   resources :users, only: [:index, :show] do 
-    resources :posts, only: [:index]
+    resources :posts, only: [:index, :show]
   
   end
 end
