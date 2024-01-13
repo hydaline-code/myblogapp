291a518bbd9b130188ac651369bc2649822ed022
hydaline-code <djougancharlen@gmail.com>
2024-01-13 19:18:54 +0100

    Add communication between all pages

--- Gemfile
+++ Gemfile
@@ -47,6 +47,7 @@ gem 'bootsnap', require: false
 gem 'rubocop', '>= 1.0', '< 2.0'
 
 gem 'rails-controller-testing'
+gem 'will_paginate', '~> 3.3'
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,12 +4,11 @@ class PostsController < ApplicationController
 
   def index
     @user = User.find(params[:user_id])
-    @posts = @user.posts.includes(:comments)
+    @posts = @user.posts.includes(:comments).paginate(page: params[:page], per_page: 2)
   end
 
   def show
-    @post = Post.find(params[:id])
-    render :show
+
   end
 
   private
