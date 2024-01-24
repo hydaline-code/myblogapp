19329e083326899b4fd4fe8c183cc1edc319c63d
hydaline-code <djougancharlen@gmail.com>
2024-01-24 19:15:46 +0100

    Capybara config with web browser

--- spec/views/posts/index.html.erb_spec.rb
+++ spec/views/posts/index.html.erb_spec.rb
@@ -1,5 +1,28 @@
+# spec/views/posts/index.html.erb_spec.rb
+
 require 'rails_helper'
 
-RSpec.describe 'posts/index.html.erb', type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'posts/index', type: :view do
+  let(:user) { User.create(name: 'Hila', photo_link: 'url', bio: 'bio', posts_counter: 0) }
+  let(:post) { user.posts.create(author_id: user.id, text: 'A post', comments_counter: 0, likes_counter: 0) }
+
+  before do
+    assign(:user, user)
+    assign(:posts, user.posts.paginate(page: 1, per_page: 2)) 
+    render
+  end
+
+  it 'displays a list of posts' do
+    expect(rendered).to have_selector('.post', count: user.posts.count)
+
+    user.posts.each do |post|
+      expect(rendered).to have_selector(".post#post_#{post.id}")
+      expect(rendered).to have_content(post.title)
+      expect(rendered).to have_content(post.text)
+    end
+  end
+
+  it 'renders pagination links' do
+    expect(rendered).to have_selector('.pagination')
+  end
 end
