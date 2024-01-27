d6733c792cea15f747c6c40f9e8624eb11c7e077
hydaline-code <djougancharlen@gmail.com>
2024-01-27 17:42:27 +0100

    Fix rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :photo_link, :bio , :posts_counter])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name photo_link bio posts_counter])
   end
-  
 end
