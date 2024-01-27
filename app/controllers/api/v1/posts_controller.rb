cfa4d22b5988208fa4fdd8022f973cf20968b0a9
hydaline-code <djougancharlen@gmail.com>
2024-01-27 20:53:20 +0100

    Fix rubocup

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::PostsController < ApplicationController
     @user = User.find(params[:user_id])
     @posts = @user.posts
     if @posts.present?
-      render json: { success: true, user: { user_id: @user.id, Name: @user.name, posts: @posts } }
+      render json: { success: true, user: { user_id: @user.id, Name: @user.Name, posts: @posts } }
     else
       render json: { success: false, message: 'No posts found' }, status: :not_found
     end
