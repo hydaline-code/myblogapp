62fc444b164cc3e7b2ce69bbe2bac63cdc54e3f0
hydaline-code <djougancharlen@gmail.com>
2024-01-27 16:21:43 +0100

    Update file

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,15 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+
+    user ||= User.new
+    if user.role == 'admin'
+      can :manage, :all
+    else
+      can :read, :all
+      can :manage, Post, author_id: user.id
+      can :manage, Comment, user_id: user.id
+    end
     # Define abilities for the user here. For example:
     #
     #   return unless user.present?
