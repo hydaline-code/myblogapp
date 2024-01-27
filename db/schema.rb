251a4d325b38ef7aaa22657e961d36bb2a98e806
hydaline-code <djougancharlen@gmail.com>
2024-01-27 15:50:12 +0100

    Add users params for signin

--- db/schema.rb
+++ db/schema.rb
@@ -48,7 +48,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_27_093239) do
     t.string "name"
     t.string "photo_link"
     t.text "bio"
-    t.integer "posts_counter"
+    t.integer "posts_counter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
